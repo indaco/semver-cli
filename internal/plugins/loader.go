4806d749311a6d6f8c72bc62b53ab16a8176d455
indaco <indaco@users.noreply.github.com>
2026-01-02 00:45:28 +0100

    feat(plugins): register tagmanager plugin in loader

--- internal/plugins/loader.go
+++ internal/plugins/loader.go
@@ -3,6 +3,7 @@ package plugins
 import (
 	"github.com/indaco/semver-cli/internal/config"
 	"github.com/indaco/semver-cli/internal/plugins/commitparser"
+	"github.com/indaco/semver-cli/internal/plugins/tagmanager"
 )
 
 func RegisterBuiltinPlugins(cfg *config.Config) {
@@ -12,5 +13,16 @@ func RegisterBuiltinPlugins(cfg *config.Config) {
 
 	if cfg.Plugins.CommitParser {
 		commitparser.Register()
+	}
+
+	if cfg.Plugins.TagManager != nil && cfg.Plugins.TagManager.Enabled {
+		tmCfg := &tagmanager.Config{
+			Enabled:    true,
+			AutoCreate: cfg.Plugins.TagManager.GetAutoCreate(),
+			Prefix:     cfg.Plugins.TagManager.GetPrefix(),
+			Annotate:   cfg.Plugins.TagManager.GetAnnotate(),
+			Push:       cfg.Plugins.TagManager.Push,
+		}
+		tagmanager.Register(tmCfg)
 	}
 }
